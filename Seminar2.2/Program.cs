d33b69a7693bbff446797e35c3585ff191906577
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-22 19:51:26 +0300

    Add Seminar2.2

---
+++ Seminar2.2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int result = number1 % number2;
+
+if(result == 0)
+   Console.WriteLine("Кратно");
+else 
+   Console.WriteLine("не кратно, остаток " + result);  
