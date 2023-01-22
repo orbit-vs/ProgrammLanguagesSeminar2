2c7fcd20cf212b6a21f16a2eeeed15ddc7259e95
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-22 20:04:13 +0300

    Seminar2.3

---
+++ Seminar2.3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число для проверки на кратность 7 и 23 ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number == 0)
+{
+    Console.WriteLine("Вы ввели 0 ");
+    return;
+}
+
+if(number % 7 == 0 && number % 23 == 0)
+   Console.WriteLine("Да");
+else
+   Console.WriteLine("Нет");
