c2d6606909ae39ffce7b9f58d843c1841f880435
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-22 20:12:00 +0300

    Seminar2.4

---
+++ Seminar2.4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число ");
+int numOne = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Второе число ");
+int numTwo = Convert.ToInt32(Console.ReadLine());
+
+double sqrtNumOne = Math.Sqrt(numOne);
+double sqrtNumTwo = Math.Sqrt(numTwo);
+
+System.Console.WriteLine(sqrtNumOne == Math.Abs(numTwo)) || sqrtNumTwo == Math.Abs(numOne);
