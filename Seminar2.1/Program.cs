0f0d5a677af78118afcb84e90955e3bca9d414c1
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-22 19:31:18 +0300

    Seminar2.1

---
+++ Seminar2.1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая выводит случайное трехзначнное число и удаляет вторую цифру этого числа
+//456 - 46
+//782 - 72
+//918 - 98
+
+
+int num = new Random().Next(10, 1000);
+
+int thirdDigit = num / 100;
+
+int firstDigit = num % 10;
+
+int result = thirdDigit * 10 + firstDigit;
+
+Console.WriteLine("Сгенерированно число " + num);
+
+Console.WriteLine("После удаления второго разряда получилось число " + result);
+
