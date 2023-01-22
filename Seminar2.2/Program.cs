e9abeffbef5a6256d22ada0e220d941ed74c4f15
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-22 19:55:19 +0300

    Added

--- Seminar2.2/Program.cs
+++ Seminar2.2/Program.cs
@@ -5,6 +5,9 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 
 int result = number1 % number2;
 
+if(number1 ==0 || number2 == 0)
+   System.Console.WriteLine("Одно из чисел является нулем ");
+
 if(result == 0)
    Console.WriteLine("Кратно");
 else 
