aec960c8fe7470e9cf786591fd43946b0d38564d
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-22 19:14:29 +0300

    Seminar2

---
+++ Seminar2/Program.cs
@@ -0,0 +1,18 @@
+﻿int number = new Random().Next(10, 100);
+
+//int firstDigit = 78 / 10;
+
+Console.WriteLine("Сгенерироавно число" + number);
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+int maxDigit = 0;
+
+if(firstDigit > secondDigit)
+   maxDigit = firstDigit;
+else
+   maxDigit = secondDigit;
+
+Console.WriteLine("Наибольшая цифра в числе " + number + ": " + maxDigit);       
+
